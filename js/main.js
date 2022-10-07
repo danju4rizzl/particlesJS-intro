f2dca1cbd093525b258805be23585589d2aa7be6
danju4rizzl <danjumashiwaju@gmail.com>
2022-10-07 13:14:45 +0200

    added starter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-/*
-particlesJS.load('<DOM ELEMENT ID>', '<PATH TO JSON CONFIG>', function () {
-	console.log('callback - particles-js config loaded')
-});
-*/
-particlesJS.load('my-particles', 'assets/particlesjs-config.json', () =>
-  console.log('Particle JS loaded 👍')
-);
+console.log('Hello World');
